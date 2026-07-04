35b13e102d2fe2031c5256be0c4d306b6c531838
Marco Cedaro <cedmax@users.noreply.github.com>
2026-07-04 22:02:16 +0100

    Add Prettier and format source files

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,5 +1,13 @@
-const shortFmt = new Intl.DateTimeFormat('it-IT', { day: 'numeric', month: 'short', year: 'numeric' })
-const longFmt  = new Intl.DateTimeFormat('it-IT', { day: 'numeric', month: 'long',  year: 'numeric' })
+const shortFmt = new Intl.DateTimeFormat("it-IT", {
+  day: "numeric",
+  month: "short",
+  year: "numeric",
+})
+const longFmt = new Intl.DateTimeFormat("it-IT", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+})
 
 export const fmtShort = (d: Date) => shortFmt.format(d)
-export const fmtLong  = (d: Date) => longFmt.format(d)
+export const fmtLong = (d: Date) => longFmt.format(d)
